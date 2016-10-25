f4e0aac274091fa5ea2dcd83920ec0b7eeea6e0a
higher <chenzhenhai@bilibili.com>
2016-10-25 17:26:42 +0800

    add 2 phpunit test of rule

--- tests/RuleTest.php
+++ tests/RuleTest.php
@@ -8,8 +8,8 @@ class RuleTest extends PHPUnit_Framework_TestCase
 
     public function testAdd()
     {
-        $user = new \Authority\Rule(['name' => 'READ_ONLY']);
-        $ret = (new \Authority\Handler())->addRule($user);
+        $rule = new \Authority\Rule(['name' => 'READ_ONLY', 'data' => 'hello world']);
+        $ret = (new \Authority\Handler())->addRule($rule);
         $this->assertEquals(\Constant::RET_OK, $ret->ret);
         if ($ret->ret == \Constant::RET_OK) {
             echo PHP_EOL, 'Add rule READ_ONLY success!', PHP_EOL;
@@ -17,6 +17,32 @@ class RuleTest extends PHPUnit_Framework_TestCase
 
         $data = json_decode($ret->data, true);
         return (int)$data['id'];
+    }
+
+    /**
+     * @depends testAdd
+     */
+    public function testGetByName($id)
+    {
+        $ret = (new \Authority\Handler())->getRuleByName('READ_ONLY');
+        $this->assertEquals($id, $ret->id);
+
+        if ($ret->id == $id) {
+            echo PHP_EOL, 'Get rule READ_ONLY by name success!', PHP_EOL;
+        }
+    }
+
+    /**
+     * @depends testAdd
+     */
+    public function testUpdate($id)
+    {
+        $rule = new \Authority\Rule(['name' => 'WRITE_ONLY']);
+        $ret = (new \Authority\Handler())->updateRule($rule, $id);
+        $this->assertEquals(\Constant::RET_OK, $ret->ret);
+        if ($ret->ret == \Constant::RET_OK) {
+            echo PHP_EOL, 'Update rule READ_ONLY to WRITE_ONLY success!', PHP_EOL;
+        }
     }
 
     /**
@@ -31,5 +57,4 @@ class RuleTest extends PHPUnit_Framework_TestCase
             echo PHP_EOL, 'Remove rule READ_ONLY success!', PHP_EOL;
         }
     }
-
 }
